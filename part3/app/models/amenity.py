90fa022dc20a62810ed5bd889e197d4bb6c8bd4d
Venghour-holberton-git <13067@holbertonstudents.com>
2026-06-30 10:27:53 +1000

    implement place to amenities relationship

--- part3/app/models/amenity.py
+++ part3/app/models/amenity.py
@@ -1,5 +1,9 @@
 from app.models.base_model import BaseModel
+from sqlalchemy import Column, Integer, String, ForeignKey
 from app.extensions import db
 
 class Amenity(BaseModel):
+    __tablename__ = "amenities"
+
     name = db.Column(db.String(128), nullable = False)
+    place_id = Column(Integer, ForeignKey="places.id", nullable = False)
--- part3/app/models/place.py
+++ part3/app/models/place.py
@@ -1,14 +1,18 @@
 from app.models.base_model import BaseModel
 from app.extensions import db
 from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.orm import relationship
 
 class Place(BaseModel):
+    __tablename__ = "places"
+
     title = db.Column(db.String(50), nullable = False)
     description = db.Column(db.String(254), nullable = False)
     price = db.Column(db.Float, nullable = False)
     latitude = db.Column(db.Float, nullable = False)
     longitude = db.Column(db.Float, nullable = False)
-    user_id = Column(Integer, ForeignKey('users.id'), nullable = False)
+    user_id = Column(Integer, ForeignKey("users.id"), nullable = False)
+    children = relationship("Amenity", backref="places", lazy=True)
 
     def add_review(self, review):
         """Add a review to the place."""
