8ab4238d170665163fd97518d66af9759a306905
Ethan-of-Aussie <13068@holbertonstudents.com>
2026-07-09 18:11:55 +1000

    saving progress for today on admins.py

--- part3/app/api/v1/admins.py
+++ part3/app/api/v1/admins.py
@@ -1,62 +1,88 @@
 #!/usr/bin/python3
+
 from app.services import facade
 from flask import request
-from flask_restx import Namespace, Resource
+from flask_restx import Namespace, Resource, fields, marshal
 from flask_jwt_extended import jwt_required, get_jwt, get_jwt_identity
 from app.extensions import bcrypt
 
 api = Namespace('admin', description='Admin operations')
 
+user_model = api.model('User', {
+    'first_name': fields.String(required=True, description='First name of the user'),
+    'last_name': fields.String(required=True, description='Last name of the user'),
+    'password': fields.String(required=True, description='User password'),
+    'email': fields.String(required=True, description='Email of the user')
+})
+
+amenity_model = api.model('Amenity', {
+    'name': fields.String(required=True, description='Name of the amenity')
+})
+
+amenity_response = api.model('AmenityResponse', {
+    'id': fields.String,
+    'name': fields.String
+})
+
 @api.route('/users/<user_id>')
 class AdminUserModify(Resource):
+    # might not need api.expect so input isnt forced only an updated field is passed
+    @api.response(400, "Invalid input data")
+    @api.response(200, "User updated successfully")
+    @api.response(404, "user not found")
+    @api.response(403, "Admin privileges required")
     @jwt_required()
     def put(self, user_id):
 
+        user_data = api.payload
+        """No data in payload fields"""
+        if not user_data:
+            return {"error": "Invalid input data"}, 400
+
         user = facade.get_user(user_id)
+        """ID was not found, wrong or missing"""
         if not user:
             return {"error": "user not found"}, 404
 
         current_user = get_jwt()
-
         # If 'is_admin' is part of the identity payload
         if not current_user.get('is_admin'):
             return {'error': 'Admin privileges required'}, 403
 
-        data = request.json
-        if not data:
-            return {"error": "No data provided"}, 400
 
-        email = data.get('email')
+        email = user_data.get('email')
+        existing_user = facade.get_user_by_email(email)
         if email:
             # Check if email is already in use
-            existing_user = facade.get_user_by_email(email)
             if existing_user and existing_user.id != user_id:
                 return {'error': 'Email is already in use'}, 400
 
-        protected = ['id', 'created_at', 'updated_at', 'is_admin']
-        for field in protected:
-            if field in data:
-                return {"error": f"field {field} cannot be updated"}, 400
+        if 'password'in user_data:
+            if user_data['password'] == "":
+                return {"error": "empty password"}, 400
+            user_data['password'] = bcrypt.generate_password_hash(user_data['password']).decode('utf-8')
 
-        if 'password'in data:
-            data['password'] = bcrypt.generate_password_hash(data['password']).decode('utf-8')
-
-        # Logic to update user details, including email and password
-
-        updated_user = facade.update_users(user_id, data)
-        return {"status": "User updated successfully"}, 200
+        updated_user = facade.update_user(user_id, user_data)
+        return {"id": updated_user.id, "message": "User updated successfully"}, 200
 
 @api.route('/users/')
 class AdminUserCreate(Resource):
+    @api.expect(user_model, validation=True)
+    @api.response(201, 'User successfully created')
+    @api.response(400, 'Invalid input data')
+    @api.response(403, "Admin privileges required")
     @jwt_required()
     def post(self):
-        current_user = get_jwt()
-        if not current_user.get('is_admin'):
-            return {'error': 'Admin privileges required'}, 403
 
-        user_data = request.json
+        user_data = api.payload
+        """No data in user fields"""
         if not user_data:
             return {"error": "No data provided"}, 400
+
+        current_user = get_jwt()
+        """Admin verification"""
+        if not current_user.get('is_admin'):
+            return {'error': 'Admin privileges required'}, 403
 
         email = user_data.get('email')
 
@@ -76,31 +102,53 @@ class AdminUserCreate(Resource):
 
 @api.route('/amenities/')
 class AdminAmenityCreate(Resource):
+    @api.expect(amenity_model, validation=True)
+    @api.response(400, "Invalid input data")
+    @api.response(201, "Amenity successfully created")
+    @api.response(403, "Admin privileges required")
     @jwt_required()
     def post(self):
+
+        amenity_data = api.payload
+        """No data in fields"""
+        if not amenity_data:
+            return {"error": "Invalid input data"}, 400
+
+        """empty string check"""
+        if "name" in amenity_data and amenity_data["name"] == "":
+            return {"error": "empty name field"}, 400
+
         current_user = get_jwt()
+        """If not admin validation"""
         if not current_user.get('is_admin'):
             return {'error': 'Admin privileges required'}, 403
-
-        amenity_data = request.json
-        if not amenity_data:
-            return {"error": "No data provided"}, 400
 
         # Logic to create a new amenity
         try:
             new_amenity = facade.create_amenity(amenity_data)
         except ValueError as e:
             return {"error": str(e)}, 400
-        return {
-            "id": new_amenity.id,
-            "name": new_amenity.name,
-            "message": "Amenity created successfully"
-        }, 201
+        return marshal(new_amenity, amenity_response), 201
 
 @api.route('/amenities/<amenity_id>')
 class AdminAmenityModify(Resource):
+    # might not need api.expect so input isnt forced only an updated field is passed
+    @api.response(200, 'Amenity updated successfully')
+    @api.response(404, 'Amenity not found')
+    @api.response(400, 'Invalid input data')
+    @api.response(403, "Admin privileges required")
     @jwt_required()
     def put(self, amenity_id):
+
+        amenity_data = api.payload
+        """No data in fields"""
+        if not amenity_data:
+            return {"error": "Invalid input data"}, 400
+
+        """empty string check"""
+        if "name" in amenity_data and amenity_data["name"] == "":
+            return {"error": "empty name field"}, 400
+
         current_user = get_jwt()
         if not current_user.get('is_admin'):
             return {'error': 'Admin privileges required'}, 403
@@ -108,10 +156,6 @@ class AdminAmenityModify(Resource):
         amenity = facade.get_amenity(amenity_id)
         if amenity is None:
             return {"error": "Amenity not found"}, 404
-
-        amenity_data = request.json
-        if not amenity_data:
-            return {"error": "No data provided"}, 400
 
         try:
             updated_amenity = facade.update_amenity(amenity_id, amenity_data)
@@ -121,26 +165,36 @@ class AdminAmenityModify(Resource):
 
 @api.route('/places/<place_id>')
 class AdminPlaceModify(Resource):
+    # might not need api.expect so input isnt forced only an updated field is passed
+    @api.response(200, 'Place updated successfully')
+    @api.response(404, 'Place not found')
+    @api.response(400, 'Invalid input data')
+    @api.response(403, "Admin privileges required")
     @jwt_required()
     def put(self, place_id):
-        current_user = get_jwt()
-        # Set is_admin default to False if not exists
-        is_admin = current_user.get('is_admin', False)
-        user_id = get_jwt_identity()
 
-        place_data = request.json
+        place_data = api.payload
+        """No data in fields"""
         if not place_data:
-            return {"error": "place not founded"}, 404
-        if place_data["title"] == "":
+            return {"error": "Invalid input data"}, 400
+
+        """Checking if title of place empty. if title is updating an empty string"""
+        if "title" in place_data and place_data["title"] == "":
             return {"error": "empty title"}, 400
 
-        place = facade.get_place(place_id)
-        if place is None:
-            return {"error": "place not founded"}, 404
+        current_user = get_jwt()
+        # Set is_admin default to False if not exists
 
+        is_admin = current_user.get('is_admin', False)
         if not is_admin:
             return {'error': 'Unauthorized action'}, 403
 
-        updated_place = facade.update_place(place_id, place_data)
+        place = facade.get_place(place_id)
+        if place is None:
+            return {"error": "place not found"}, 404
 
+        updated_place = facade.update_place(place_id, place_data)
+        """Update fail"""
+        if updated_place is None:
+            return {"error": "update failed"}, 400
         return {"message": "Place updated successfully"}, 200
